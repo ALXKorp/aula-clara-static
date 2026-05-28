63583332fe4ec15eff267b2e0e2827f6c7be3aad
Alejandro Herrera <al.x.streaming20@gmail.com>
2026-05-28 13:33:30 +0200

    Configurar basePath para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
 const nextConfig = (phase: string): NextConfig => ({
   output: "export",
+  basePath: "/aula-clara-static",
+  assetPrefix: "/aula-clara-static/",
+  trailingSlash: true,
   distDir: phase === PHASE_DEVELOPMENT_SERVER ? ".next-dev" : ".next"
 });
 
